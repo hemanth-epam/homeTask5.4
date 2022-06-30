6b3280e787c1ba0e7e793560d37ab4a8a2f25b57
Hemanth Reddy <Hemanth_Reddy_Sambavaram@epam.com>
2022-06-30 11:32:39 +0530

    Updated

--- routers/router.js
+++ routers/router.js
@@ -8,7 +8,7 @@ const userValidator = require("../services/userService.js").userValidator;
 
 router.get('/users', userController.getUsers);    //all users
 router.get('/users/:id', userController.getUsersById);
-router.post('/users', userValidator(userSchema), userController.saveUser);   //save user
+router.post('/users',userValidator(userSchema), userController.saveUser);   //save user
 router.put('/users/:id', userValidator(userSchema), userController.updateUser); //update by id
 router.delete('/users/:id', userController.deleteUser); //delete by id
 module.exports = {
